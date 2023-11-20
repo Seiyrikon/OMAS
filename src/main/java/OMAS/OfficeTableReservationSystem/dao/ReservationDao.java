ab3af3bd7dba8a511b13bc91109f67143caa1abf
Ricky Galpo <rickygalpo7777@gmail.com>
2023-11-20 11:33:38 +0800

    Created API for saving reservation

---
+++ src/main/java/OMAS/OfficeTableReservationSystem/dao/ReservationDao.java
@@ -0,0 +1,11 @@
+package OMAS.OfficeTableReservationSystem.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import OMAS.OfficeTableReservationSystem.model.Reservation;
+
+@Mapper
+public interface ReservationDao {
+    void save(Reservation body);
+
+}
